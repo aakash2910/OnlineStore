1619b904ec1e85cbb315c357dda52dabe957a96a
aakash <aakash.jiyani@yahoo.ca>
2023-01-25 23:50:50 -0600

    Added Product Model

---
+++ OnlineStore/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required(ErrorMessage = "Product Name is required")]
+        [MaxLength(50, ErrorMessage = "Maximum 50 Characters allowed")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Product description is required")]
+        public string Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public long Price { get; set; }
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
